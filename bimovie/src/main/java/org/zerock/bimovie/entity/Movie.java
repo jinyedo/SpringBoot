deeaaf3e2dafcc3d593ee1dd905af13113270c18
jinyedo <whereyedo@naver.com>
2021-03-21 00:26:16 +0900

    @OneToMany 지정 Movie(One) : Poster(Many)

--- bimovie/src/main/java/org/zerock/bimovie/entity/Movie.java
+++ bimovie/src/main/java/org/zerock/bimovie/entity/Movie.java
@@ -3,13 +3,15 @@ package org.zerock.bimovie.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-@ToString
+@ToString(exclude = "posterList")
 public class Movie extends BaseEntity {
 
     @Id
@@ -17,5 +19,8 @@ public class Movie extends BaseEntity {
     private Long mno;
 
     private String title;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Poster> posterList = new ArrayList<>();
 }
 
