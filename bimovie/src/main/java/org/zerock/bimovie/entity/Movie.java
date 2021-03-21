ef66af260b085d2d22f1d14abafbb835aff2c909
jinyedo <whereyedo@naver.com>
2021-03-21 22:07:42 +0900

    Poster 를 추가하는 기능 추가

--- bimovie/src/main/java/org/zerock/bimovie/entity/Movie.java
+++ bimovie/src/main/java/org/zerock/bimovie/entity/Movie.java
@@ -20,7 +20,16 @@ public class Movie extends BaseEntity {
 
     private String title;
 
+    @Builder.Default // Movie 객체가 JPA 에서 엔티티 매니저에 의해서 관리될 때 변수에 할당된 ArrayList 객체도 같이 보관하도록
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "movie")
     private List<Poster> posterList = new ArrayList<>();
+
+    // Poster 추가
+    // Poster 객체를 파라미터로 받아 Poster 객체의 Movie 를 현재 객체로 지정
+    public void addPoster(Poster poster) {
+        poster.setIdx(this.posterList.size());
+        poster.setMovie(this);
+        posterList.add(poster);
+    }
 }
 
