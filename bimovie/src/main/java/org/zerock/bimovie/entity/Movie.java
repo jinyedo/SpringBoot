ce0fdb4389397baf62704e1edee42430480a4add
jinyedo <whereyedo@naver.com>
2021-03-23 12:36:09 +0900

    Poster 에 모든 상태 변화 전파

--- bimovie/src/main/java/org/zerock/bimovie/entity/Movie.java
+++ bimovie/src/main/java/org/zerock/bimovie/entity/Movie.java
@@ -21,7 +21,7 @@ public class Movie extends BaseEntity {
     private String title;
 
     @Builder.Default // Movie 객체가 JPA 에서 엔티티 매니저에 의해서 관리될 때 변수에 할당된 ArrayList 객체도 같이 보관하도록
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "movie")
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "movie", cascade = CascadeType.ALL)
     private List<Poster> posterList = new ArrayList<>();
 
     // Poster 추가
