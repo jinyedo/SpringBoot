fbcfdf08eb39bd1fa8e97dad005956136ee42aa6
jinyedo <whereyedo@naver.com>
2021-03-23 20:38:52 +0900

    PagingTest4

--- bimovie/src/main/java/org/zerock/bimovie/repository/MovieRepository.java
+++ bimovie/src/main/java/org/zerock/bimovie/repository/MovieRepository.java
@@ -13,8 +13,12 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
     @Query("SELECT m FROM Movie m")
     Page<Movie> findAll2(Pageable pageable);
 
-    @Query("SELECT m, p, count(p) " +
+//    @Query("SELECT m, p, count(p) " +
+//            "FROM Movie m LEFT JOIN Poster p ON p.movie = m " +
+//            "GROUP BY p.movie")
+    @Query("SELECT m, p, COUNT(p) " +
             "FROM Movie m LEFT JOIN Poster p ON p.movie = m " +
+            "WHERE p.idx = 1 " +
             "GROUP BY p.movie")
     Page<Object[]> findAll3(Pageable pageable);
 }
