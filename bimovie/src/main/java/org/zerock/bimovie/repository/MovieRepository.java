a8ce4a0c04319a82f39f7957c08268fadb693701
jinyedo <whereyedo@naver.com>
2021-03-23 19:35:29 +0900

    @EntityGraph 를 이용한 페이징 처리

--- bimovie/src/main/java/org/zerock/bimovie/repository/MovieRepository.java
+++ bimovie/src/main/java/org/zerock/bimovie/repository/MovieRepository.java
@@ -1,8 +1,16 @@
 package org.zerock.bimovie.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.zerock.bimovie.entity.Movie;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    @EntityGraph(attributePaths = "posterList", type = EntityGraph.EntityGraphType.LOAD)
+    @Query("SELECT m FROM Movie m")
+    Page<Movie> findAll2(Pageable pageable);
 }
 
