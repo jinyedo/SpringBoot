c4ad716644690c6aa10e328eb4fcb0146286d3e1
jinyedo <whereyedo@naver.com>
2021-03-23 18:26:43 +0900

    orphanRemoval = true 지정

--- bimovie/src/main/java/org/zerock/bimovie/entity/Movie.java
+++ bimovie/src/main/java/org/zerock/bimovie/entity/Movie.java
@@ -22,7 +22,7 @@ public class Movie extends BaseEntity {
     private String title;
 
     @Builder.Default // Movie 객체가 JPA 에서 엔티티 매니저에 의해서 관리될 때 변수에 할당된 ArrayList 객체도 같이 보관하도록
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "movie", cascade = CascadeType.ALL)
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "movie", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Poster> posterList = new ArrayList<>();
 
     // Poster 추가
